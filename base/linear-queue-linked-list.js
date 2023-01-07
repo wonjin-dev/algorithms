da5ee56ea8f8cb240af5c12ef0bc03cb460ef2b2
wonjin-dev <fsdjin159@gmail.com>
2023-01-07 23:42:27 +0900

    큐

---
+++ base/linear-queue-linked-list.js
@@ -0,0 +1,46 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor() {
+		this.head = null;
+		this.tail = size;
+		this.size = 0;
+	}
+
+	enqueue(newValue) {
+		const newNode = new Node(newValue);
+		if (this.head === null) {
+			this.head = this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.size += 1;
+	}
+
+	dequeue() {
+		const value = this.head.value;
+		this.head = this.head.next;
+		this.size -= 1;
+
+		return value;
+	}
+
+	peek() {
+		return this.head.value;
+	}
+}
+
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(4);
+queue.enqueue(8);
+queue.size; // 3
+queue.peek(); // 2
