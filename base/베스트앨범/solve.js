73e78765b7f7f9435a54bcf4931833194ad2fdb6
wonjin-dev <fsdjin159@gmail.com>
2023-01-09 01:42:27 +0900

    [베스트앨범] 1차 풀이

---
+++ base/베스트앨범/solve.js
@@ -0,0 +1,41 @@
+function solution(genres, plays) {
+	const g = [...new Set(genres)];
+	const countArr = g.map((g) => {
+		return {
+			genre: g,
+			count: 0,
+			index: [],
+		};
+	});
+
+	const musics = [];
+	for (let i = 0; i < genres.length; i++) {
+		musics.push({
+			genre: genres[i],
+			play: plays[i],
+			index: i,
+		});
+	}
+
+	musics.map((arr) => {
+		const i = countArr.findIndex((a) => a.genre === arr.genre);
+		countArr[i].count = countArr[i].count + arr.play;
+		countArr[i].index.push({play: arr.play, i: arr.index});
+	});
+	console.log(countArr);
+
+	const sorted = countArr.sort((a, b) => b.count - a.count);
+
+	const parse = sorted
+		.map((arr) => arr.index.sort((a, b) => b.play - a.play).slice(0, 2))
+		.flat();
+
+	const answer = parse.flatMap((a) => a.i);
+
+	return answer;
+}
+
+// solution(
+// 	['classic', 'pop', 'classic', 'classic', 'pop'],
+// 	[500, 600, 150, 800, 2500]
+// );
