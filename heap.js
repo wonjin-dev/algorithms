42582e35eec445e3475be3a08bc2312d03e8e49f
wonjin-dev <fsdjin159@gmail.com>
2023-01-21 20:47:50 +0900

    힙

---
+++ heap.js
@@ -0,0 +1,51 @@
+class MaxHeap {
+	constructor() {
+		this.heap = [null];
+	}
+
+	push(value) {
+		this.heap.push(value);
+		let currentIndex = this.heap.length - 1;
+		let parentIndex = Math.floor(currentIndex / 2);
+
+		while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+			const temp = this.heap[parentIndex];
+
+			this.heap[parentIndex] = value;
+			this.heap[currentIndex] = temp;
+
+			currentIndex = parentIndex;
+			parentIndex = Math.floor(currentIndex / 2);
+		}
+	}
+
+	pop() {
+		const returnValue = this.heap[1];
+		this.heap[1] = this.heap.pop();
+
+		let currentIndex = 1;
+		let leftIndex = 2;
+		let rightIndex = 3;
+		while (
+			this.heap[currentIndex] < this.heap[leftIndex] ||
+			this.heap[currentIndex] < this.heap[rightIndex]
+		) {
+			const temp = this.heap[currentIndex];
+
+			if (this.heap[leftIndex] < this.heap[rightIndex]) {
+				this.heap[currentIndex] = this.heap[rightIndex];
+				this.heap[rightIndex] = temp;
+
+				currentIndex = rightIndex;
+			} else {
+				this.heap[currentIndex] = this.heap[leftIndex];
+				this.heap[leftIndex] = temp;
+				currentIndex = leftIndex;
+			}
+			leftIndex = currentIndex * 2;
+			rightIndex = currentIndex * 2 + 1;
+		}
+
+		return returnValue;
+	}
+}
