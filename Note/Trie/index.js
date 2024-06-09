6e840f19e3c7bfd9662e83cb59650fbd5e4cac23
wonjin-dev <fsdjin159@gmail.com>
2024-06-09 22:07:54 +0900

    [Trie] add snippet for Trie

---
+++ Note/Trie/index.js
@@ -0,0 +1,37 @@
+class Node {
+	constructor(value = '') {
+		this.value = value;
+		this.children = new Map();
+	}
+}
+
+class Trie {
+	constructor() {
+		this.root = new Node();
+	}
+
+	insert(string) {
+		let currentNode = this.root;
+
+		for (const char of string) {
+			if (!currentNode.chilren.has(char)) {
+				currentNode.children.set(char, new Node((currentNode.value = char)));
+			}
+		}
+
+		currentNode = currentNode.children.get(char);
+	}
+
+	has(string) {
+		let currentNode = this.root;
+
+		for (const char of string) {
+			if (!currentNode.children.has(char)) {
+				return false;
+			}
+			currentNode = currentNode.children.get(char);
+		}
+
+		return true;
+	}
+}
